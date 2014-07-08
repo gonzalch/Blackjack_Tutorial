ab44b3e6798e845d5d8cbfce1e976bcfc0400104
chrisgonzalez <chrisg533@gmail.com>
2014-07-07 21:12:01 -0700

    Added BlackjackGame class to handle game logic. Not currently used.

--- bt/src/main/java/com/gonzalch/blackjacktutorial/bt/BlackjackGame.java
+++ bt/src/main/java/com/gonzalch/blackjacktutorial/bt/BlackjackGame.java
@@ -8,7 +8,11 @@ package com.gonzalch.blackjacktutorial.bt;
     well as player and dealers hands and includes methods to start a new round of Blackjack, deal initial
     cards, allow player to hit, split, double, stand, and surrender.
  */
+
+import android.util.Log;
+
 public class BlackjackGame {
+    private static final String TAG = "BlackjackGame";
     private BlackjackHand playerHand;
     private BlackjackHand dealerHand;
     private Deck deck;
@@ -16,15 +20,19 @@ public class BlackjackGame {
     private Boolean inProgress = false;
 
     public BlackjackGame() {
-        if (playerHand == null)
+        if (playerHand == null) {
             playerHand = new BlackjackHand();
-        else
+        }
+        else {
             playerHand.clear();
+        }
 
-        if (dealerHand == null)
+        if (dealerHand == null) {
             dealerHand = new BlackjackHand();
-        else
+        }
+        else {
             dealerHand.clear();
+        }
 
         if (deck == null)
             deck = new Deck();
@@ -38,16 +46,23 @@ public class BlackjackGame {
 
     public void dealCards() {
         // Deal cards from deck into player and dealer hands.
+        Log.d(TAG, "Dealing Cards");
 
         if (deck.cardsLeft() < 26) {
             deck = new Deck();
             deck.shuffle();
         }
 
-
-
         playerHand.addCard(deck.dealCard());
         dealerHand.addCard(deck.dealCard());
+        playerHand.addCard(deck.dealCard());
+        dealerHand.addCard(deck.dealCard());
+
+        Log.d(TAG, "Player: " + playerHand.getCard(0).getValueAsString() + " of " + playerHand.getCard(0).getSuitAsString() + " dealt.");
+        Log.d(TAG, "Player: " + playerHand.getCard(1).getValueAsString() + " of " + playerHand.getCard(1).getSuitAsString() + " dealt.");
+
+        Log.d(TAG, "Dealer: " + dealerHand.getCard(0).getValueAsString() + " of " + dealerHand.getCard(0).getSuitAsString() + " dealt.");
+        Log.d(TAG, "Dealer: " + dealerHand.getCard(1).getValueAsString() + " of " + dealerHand.getCard(1).getSuitAsString() + " dealt.");
 
     }
 
@@ -60,11 +75,25 @@ public class BlackjackGame {
         // Add card into players hand.
         playerHand.addCard(deck.dealCard());
 
+        Log.d(TAG, "Player: " + playerHand.getCard(playerHand.getCardCount()).getValueAsString() + " of " + playerHand.getCard(playerHand.getCardCount()).getSuitAsString() + " dealt.");
+
         // Check if player has busted. If true, game ends and player loses.
         // if (playerHand.getBlackjackValue() > 21)
 
         // Check if player has 21. If true, game ends and check if player wins.
         // if (playerHand.getBlackjackValue() == 21)
 
+    }
+
+    public BlackjackHand getPlayerHand() {
+        return playerHand;
+    }
+
+    public BlackjackHand getDealerHand() {
+        return dealerHand;
+    }
+
+    public Deck getDeck() {
+        return deck;
     }
 }
